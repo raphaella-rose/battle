4b5cd6eebe45f8753a043cc5968ec515ef31a337
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-05-31 18:47:00 +0100

    created project structure and route

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+  get '/' do
+    'hello, Battle!'
+  end
+  run! if app_file == $0
+end
